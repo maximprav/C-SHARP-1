af1105cdf81018724f613a0f2b2438c0b34d47d0
Maxim Prav <mxmlknv@mail.ru>
2023-02-05 09:37:37 +0300

    Добавили пример с суммой двух чисел

---
+++ Examples003_sum/Program.cs
@@ -0,0 +1,7 @@
+﻿// Создать программу для вычисления суммы двух целых чисел
+
+Console.Clear();
+int numberA = 3;
+int numberB = 4;
+// Можно было ввести дополнительную переменную int result = numberA + numberB
+Console.Write(numberA+numberB);
