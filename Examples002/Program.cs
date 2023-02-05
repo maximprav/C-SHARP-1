3519375d016138f7bbe12f5123073bfdd47277b6
Maxim Prav <mxmlknv@mail.ru>
2023-02-05 09:29:15 +0300

    Добавили две папки с примерами

---
+++ Examples002/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+// Строка для считывания данных. Чтобы считать строку добавляем в начале string.
+Console.WriteLine("Привет, " + username);
