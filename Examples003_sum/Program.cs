6440a00d72bddd555743eecbc4d96c5c13e2c992
Maxim Prav <mxmlknv@mail.ru>
2023-02-05 09:48:16 +0300

    Внесли правки в пример суммы - добаваили случайные числа

--- Examples003_sum/Program.cs
+++ Examples003_sum/Program.cs
@@ -1,7 +1,9 @@
 ﻿// Создать программу для вычисления суммы двух целых чисел
 
 Console.Clear();
-int numberA = 3;
-int numberB = 4;
+int numberA = new Random().Next(1,10); //1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 // Можно было ввести дополнительную переменную int result = numberA + numberB
 Console.Write(numberA+numberB);
