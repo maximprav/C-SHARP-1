74c6820214c90d0a11f8b4bd1f13d9e7bb372a57
Maxim Prav <mxmlknv@mail.ru>
2023-02-05 10:02:12 +0300

    Добавили пример различного приветствия юзера

---
+++ Examples005_Hello/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите ваше имя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, " + username + "!" + " Хорошего дня!");
+}
+else 
+{
+    Console.WriteLine("Привет, " + username + "!");
+}
